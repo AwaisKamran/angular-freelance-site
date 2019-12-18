dc3d3f17beef0b6918e2b99c10b1747577517982
Awais Kamran <awais.kamran@hotmail.com>
2019-12-17 20:47:00 -0800

    angular-freelance-site: review component updated

--- src/app/services/constants.service.ts
+++ src/app/services/constants.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConstantsService {
-  public url: any = 'http://localhost/freelance-management/';
-  //public url: any = 'http://freelance.freelancehouseservices.com/awais/freelance-management/';
+  //public url: any = 'http://localhost/freelance-management/';
+  public url: any = 'http://freelance.freelancehouseservices.com/awais/freelance-management/';
   constructor() { }
 
   getImageUrl(id){
